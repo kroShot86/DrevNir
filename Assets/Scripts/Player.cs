bfa3a6dc97ec3947da9e7e7636db3d345a9be692
”kroShot” <kroShot86@mail.ru>
2026-02-04 14:37:32 +1000

    Ещё анима

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,54 +7,29 @@ public class Player : MonoBehaviour
 
     public static Player Instance {  get; private set; }
 
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 1f;
     private Rigidbody2D rb;
-
-    private int num_anim = 0;
 
     private void Awake()
     {
         Instance = this;
         rb = GetComponent<Rigidbody2D>();
     }
-    //
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
     private void HandleMovement()
     {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
         inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + inputVector * (speed * Time.fixedDeltaTime));
-
-
-        if ((inputVector.y > 0.1 && (Mathf.Abs(inputVector.x) > 0.1)) || (inputVector.y > 0.1))
-        {
-            num_anim = 1; // вверх
-        }
-        else if ((inputVector.y < -0.1 && (Mathf.Abs(inputVector.x) > 0.1)) || (inputVector.y < -0.1))
-        {
-            num_anim = 2; //вниз
-        }
-        else if(inputVector.x < -0.1)
-        {
-            num_anim = 3; //лево
-        }
-        else if (inputVector.x > 0.1)
-        {
-            num_anim = 4; //вправо
-        }
-        else
-        {
-            num_anim = 0; // на месте
-        }
-
     }
 
-    public int Num_anim()
+    public Vector2 Player_pos()
     {
-        return num_anim;
-    }
-
-    private void FixedUpdate()
-    {
-        HandleMovement();
+        return GameInput.Instance.GetMovementVector();
     }
 }
--- Assets/Scripts/PlayerVisual.cs
+++ Assets/Scripts/PlayerVisual.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 public class PlayerVisual : MonoBehaviour
 {
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private int num_anim = 0;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        animator.SetInteger("num_anim", Player.Instance.Num_anim());
+        animator.SetInteger("num_anim", num_anim);
+        Animation();
     }
+
+    private void Animation()
+    {
+        Vector2 inputVector = Player.Instance.Player_pos();
+
+        if ((inputVector.y > 0.1 && (Mathf.Abs(inputVector.x) > 0.1)) || (inputVector.y > 0.1))
+        {
+            spriteRenderer.flipX = false;
+            num_anim = 1; // вверх
+        }
+        else if ((inputVector.y < -0.1 && (Mathf.Abs(inputVector.x) > 0.1)) || (inputVector.y < -0.1))
+        {
+            spriteRenderer.flipX = false;
+            num_anim = 2; //вниз
+        }
+        else if (inputVector.x < -0.1)
+        {
+            spriteRenderer.flipX = true;
+            num_anim = 3; //лево
+        }
+        else if (inputVector.x > 0.1)
+        {
+            spriteRenderer.flipX = false;
+            num_anim = 3; //вправо
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+            num_anim = 0; // на месте
+        }
+    }
+
 }
