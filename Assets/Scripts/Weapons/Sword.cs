a75a20ae07c85d34ddba5e024f0100d77137a6e6
”kroShot” <kroShot86@mail.ru>
2026-02-06 18:39:17 +1000

    возможность уничтожения врага

--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -5,10 +5,50 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    [SerializeField] private int damage = 5;
 
     public event EventHandler OnSwordSwing;
+
+    private PolygonCollider2D polygonCollider2D;
+
+    private void Awake()
+    {
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+    }
+
+    private void Start()
+    {
+        AttackColiderTurnOff();
+    }
+
     public void Attack()
     {
+        AttackColiderTurnOffOn();
         OnSwordSwing?.Invoke(this, EventArgs.Empty);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.TryGetComponent(out EnemyEnity enemyEntity))
+        {
+            enemyEntity.TakeDamage(5);
+        }
+    }
+
+    public void AttackColiderTurnOff()
+    {
+        polygonCollider2D.enabled = false;
+    }
+
+    public void AttackColiderTurnOn()
+    {
+        polygonCollider2D.enabled = true;
+    }
+
+    public void AttackColiderTurnOffOn()
+    {
+        AttackColiderTurnOff();
+        AttackColiderTurnOn();
     }
 }
