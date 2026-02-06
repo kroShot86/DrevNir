a84fea4bb51a14a2bba656904ee3056ce5a88d4c
”kroShot” <kroShot86@mail.ru>
2026-02-06 19:42:12 +1000

    частично добавлен режим преследования игрока

--- Assets/Scripts/Slime/MobAi.cs
+++ Assets/Scripts/Slime/MobAi.cs
@@ -4,6 +4,7 @@ using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.AI;
 using KnightAdventure.Utils;
+using Unity.VisualScripting;
 
 public class MobAi : MonoBehaviour
 {
@@ -12,16 +13,41 @@ public class MobAi : MonoBehaviour
     [SerializeField] private float MinDistance;
     [SerializeField] private float MaxTimePatrull;
 
+    [SerializeField] private bool isChasingEnemy = false;
+    private float chasingDistance = 10;
+    private float chasingSpeedMultiplayer = 2;
+
     private NavMeshAgent agent;
-    private State state;
+    private State currentState;
     private float currentTimePatrull;
     private Vector3 PatrullPos;
     private Vector3 startPos;
 
+    private float patrollSpeed;
+    private float chasingSpeed;
+
+    
+
+    public bool isRunning()
+    {
+        if (agent.velocity == Vector3.zero)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
     private enum State
     {
         Idle,
-        Patroll
+        Patroll,
+        Chasing,
+        Attacking,
+        Death
     }
 
     private void Awake()
@@ -29,7 +55,9 @@ public class MobAi : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        state = StartState;
+        currentState = StartState;
+        patrollSpeed = agent.speed;
+        chasingSpeed = agent.speed * chasingSpeedMultiplayer;
     }
 
     private void Start()
@@ -39,11 +67,14 @@ public class MobAi : MonoBehaviour
 
     private void Update()
     {
-        switch (state)
+        StateHandler();
+    }
+
+    private void StateHandler()
+    {
+        switch (currentState)
         {
-            default:
-            case State.Idle:
-                break;
+
             case State.Patroll:
                 currentTimePatrull -= Time.deltaTime;
 
@@ -54,8 +85,63 @@ public class MobAi : MonoBehaviour
                 }
 
                 break;
+
+            case State.Chasing:
+                ChasingTarget();
+                CheckCurrentState();
+                break;
+
+            case State.Death:
+                break;
+
+            case State.Attacking:
+                break;
+
+            default:
+            case State.Idle:
+                break;
+
         }
     }
+
+    private void CheckCurrentState()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.Instance.transform.position);
+        State newState = State.Patroll;
+
+        if (isChasingEnemy)
+        {
+            if(distanceToPlayer <= chasingDistance)
+            {
+                newState = State.Chasing;
+            }
+        }
+
+        if(newState != currentState)
+        {
+            if(newState == State.Chasing)
+            {
+                agent.ResetPath();
+                agent.speed = chasingSpeed;
+            }
+            else if (newState == State.Patroll)
+            {
+                currentTimePatrull = 0;
+                agent.speed = patrollSpeed;
+            }
+
+            currentState = newState;
+        }
+        
+
+    }
+
+
+    private void ChasingTarget()
+    {
+        agent.SetDestination(Player.Instance.transform.position);
+    }
+
 
     private void Patrull()
     {
